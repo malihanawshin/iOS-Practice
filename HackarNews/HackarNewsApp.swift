1c5d6cc055e7a9bc13550739aa4ad598cec92fcb
Maliha Nawshin Rahman <malihanawshin@gmail.com>
2023-12-20 09:26:17 -0800

    Initial Commit

---
+++ HackarNews/HackarNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackarNewsApp.swift
+//  HackarNews
+//
+//  Created by Maliha on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HackarNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
